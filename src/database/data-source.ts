af24fa9778b9f426a238af8ca2a12c7a3b96804a
Daniel Morais <ddan.mm.16@gmail.com>
2025-08-06 00:56:13 -0300

    Refactor data source configuration for environment-specific entity and migration paths

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,13 +1,21 @@
 import 'dotenv/config';
 import 'reflect-metadata';
+import path from 'path';
 import { DataSource } from 'typeorm';
 import { GuestEntity } from '../entities/guest';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   synchronize: false,
   logging: false,
-  entities: [GuestEntity],
-  migrations: ['src/migrations/**/*.ts'], // ajuste o caminho se necessário
+  entities: isProd
+    ? [path.join(__dirname, '../entities/*.js')] // aponta para JS compilado
+    : [GuestEntity], // no dev importa direto a classe
+  migrations: isProd
+    ? [path.join(__dirname, '../migrations/*.js')]
+    : ['src/migrations/**/*.ts'],
+  ssl: isProd ? { rejectUnauthorized: false } : false, // caso precise SSL
 });
