c2aef37f36a4524060a741884cff401aa6c609ba
Daniel Morais <ddan.mm.16@gmail.com>
2025-12-04 17:13:37 -0300

    Refactor admin login logic to include error logging and add logger mechanism

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,8 +1,7 @@
 import { Router } from 'express';
 
 import jwt from 'jsonwebtoken';
-
-import { authMiddleware } from '../middlewares/auth';
+import { logError } from '../mechanisms/logger';
 
 const router = Router();
 
@@ -11,22 +10,29 @@ const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 router.post('/login', (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  if (
-    email !== ADMIN_EMAIL ||
-    password !== ADMIN_PASSWORD ||
-    !email ||
-    !password
-  ) {
-    return res.status(401).json({ error: 'Credenciais inválidas' });
+    if (
+      email !== ADMIN_EMAIL ||
+      password !== ADMIN_PASSWORD ||
+      !email ||
+      !password
+    ) {
+      return res.status(401).json({ error: 'Credenciais inválidas' });
+    }
+
+    const token = jwt.sign({ role: 'admin' }, process.env.JWT_SECRET!, {
+      expiresIn: '12h',
+    });
+
+    return res.json({ token });
+  } catch (err: any) {
+    logError(`Erro no login do admin: ${err.message}`);
+    return res
+      .status(500)
+      .json({ error: 'Erro no login do admin', message: err.message });
   }
-
-  const token = jwt.sign({ role: 'admin' }, process.env.JWT_SECRET!, {
-    expiresIn: '12h',
-  });
-
-  return res.json({ token });
 });
 
 export default router;
